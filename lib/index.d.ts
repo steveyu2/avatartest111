503643aa1c61f0ecef6fabb274954342ee42cbe5
steve <evets@foxmail.com>
2025-01-13 18:20:43 +0800

    12e12e

---
+++ lib/index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * Do not change this file manually! This file was generated with the "Dicebear Exporter"-Plugin for Figma.
+ *
+ * Plugin: https://www.figma.com/community/plugin/1005765655729342787
+ */
+/*!
+ * avatar_test (@dicebear/avatartest)
+ *
+ * Code licensed under MIT License.
+ * Copyright (c) 2025 Florian Körner
+ *
+ * Design "avatar_test" by byd licensed under CC BY 4.0. / Remix of the original.
+ * License: https://creativecommons.org/licenses/by/4.0/
+ */
+import type { StyleCreate, StyleMeta } from '@dicebear/core';
+import type { Options } from './types.js';
+export declare const meta: StyleMeta;
+export declare const create: StyleCreate<Options>;
+export { schema } from './schema.js';
+export type { Options } from './types.js';
