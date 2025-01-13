039d1b58883bb708d2bebcf0d88929cd40eeb43a
steve <evets@foxmail.com>
2025-01-13 18:32:12 +0800

    e23e

--- pages/create.js
+++ pages/create.js
@@ -14,22 +14,11 @@ export async function getServerSideProps(context) {
   const id = context.query?.id || '';
   
   console.log('context.params',  context.query);
-  if(!!id){
+   
 
-  const svgPath = path.resolve(__dirname, '../../../public/avatar', `${id}.svg`);
-
-  if (false === fs.existsSync(svgPath)) {
-    if (false === fs.existsSync(path.dirname(svgPath))) {
-      fs.mkdirSync(path.dirname(svgPath), { recursive: true });
-    }
-
-    // fs.writeFileSync(svgPath, createAvatar(style, { seed: id }).toString());
-  }
-  console.log('context.params',  svgPath);
-  }
   return {
     props: {
-      svg:createAvatar(style, { seed: id }).toString()
+      svg:!!id?createAvatar(style, { seed: id }).toString():''
     }, // will be passed to the page component as props
   };
 }
