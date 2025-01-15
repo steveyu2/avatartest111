c02903e72f9e3e4d6d1da7246f20e1d9eb990bf7
steve <evets@foxmail.com>
2025-01-15 23:31:57 +0800

    21e

--- scripts/build-json.js
+++ scripts/build-json.js
@@ -1,14 +1,20 @@
+const { v7: uuidv4 } = require('uuid');
+
 (async () => {
   const { createAvatar } = await import('@dicebear/core');
   const style = await import('../avatar/lib/index.js');
   const path = require('path');
   const fs = require('fs');
 
+  /** 用json文件数组 */
   const jsonFile = fs.readFileSync(
     path.resolve(__dirname, 'json.json'),
     'utf8'
   );
   const data = JSON.parse(jsonFile);
+
+  /** 用随机id生成 */
+  // const data=Array.from({length:10000}).map((v,i)=>uuidv4());
 
   data.forEach((seed) => {
     const svgPath = path.resolve(__dirname, 'build/avatar', `${seed}.svg`);
